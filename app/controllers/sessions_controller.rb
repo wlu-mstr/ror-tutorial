b9c0850d15bf7c4addd7dd8967f1dbad2b0585ea
wlu-mstr <wlu@.pclc0.merkle.local>
2014-07-30 17:18:55 +0800

    work on user log in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
     
   end
   
+  # this is called from form_for session_path in new.html.erb
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Sign the user in and redirect to the user's show page.
+      sign_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
   end
   
   def destroy
