3863d3b7fe3913203e8238d8b2c9e9263d3e46a1
luwei <luwei@luwei-Latitude-D610.(none)>
2014-07-31 22:48:17 +0800

    add micropost module and relationship with user. will stop here...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   # for user who logged in to see all users
